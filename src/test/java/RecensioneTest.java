ec5a197424e838447e282795cde095f953c37e5f
gerryos95 <espositogerardo@live.it>
2023-02-14 12:50:19 +0100

    test RecensioneTest

--- src/test/java/RecensioneTest.java
+++ src/test/java/RecensioneTest.java
@@ -1,4 +1,5 @@
 import gestioneProdotto.service.GestioneProdottoServiceImp;
+import model.dao.review.SqlReviewDao;
 import model.entity.Prodotto;
 import model.entity.Review;
 import org.junit.jupiter.api.BeforeEach;
@@ -6,7 +7,10 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import validate.ValidateForm;
 
+import java.sql.SQLException;
+
 import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
 
 public class RecensioneTest {
 
@@ -25,26 +29,46 @@ public class RecensioneTest {
 
         ValidateForm mockValidateForm = Mockito.mock(ValidateForm.class);
         Mockito.when(mockValidateForm.validateTitoloReview(titolo)).thenReturn(false);
-        assertFalse(mockValidateForm.validateTitoloReview(titolo));
+        Mockito.when(mockValidateForm.validateCommentoReview(commento)).thenReturn(true);
+        boolean titoloError=mockValidateForm.validateTitoloReview(titolo);
+        boolean commetoSuccess=mockValidateForm.validateCommentoReview(commento);
+        assertTrue(commetoSuccess);
+        assertFalse(titoloError);
     }
 
     @Test
     public void CommentoNonCorrettoTest() {
-        String titolo = "";
-        String commento = "Ottimo prodotto ad un prezzo conveniente.";
+        String titolo = "Ottimo";
+        String commento = "";
 
         ValidateForm mockValidateForm = Mockito.mock(ValidateForm.class);
-        Mockito.when(mockValidateForm.validateTitoloReview(titolo)).thenReturn(false);
-        assertFalse(mockValidateForm.validateTitoloReview(titolo));
-
-
+        Mockito.when(mockValidateForm.validateTitoloReview(titolo)).thenReturn(true);
+        Mockito.when(mockValidateForm.validateCommentoReview(commento)).thenReturn(false);
+        boolean titoloSuccess=mockValidateForm.validateTitoloReview(titolo);
+        boolean commentoError=mockValidateForm.validateCommentoReview(commento);
+        assertTrue(titoloSuccess);
+        assertFalse(commentoError);
     }
 
     @Test
-    public void RecensioeCorrettaTest() {
-        String titolo = "";
+    public void RecensioeCorrettaTest() throws SQLException {
+        String titolo = "Ottimo";
         String commento = "Ottimo prodotto ad un prezzo conveniente.";
 
+
+        ValidateForm mockValidateForm = Mockito.mock(ValidateForm.class);
+        Mockito.when(mockValidateForm.validateTitoloReview(titolo)).thenReturn(true);
+        Mockito.when(mockValidateForm.validateCommentoReview(commento)).thenReturn(true);
+
+        Review  review=new Review();
+        review.setTitolo(titolo);
+        review.setDescrizione(commento);
+
+
+        SqlReviewDao mockReviewDao=Mockito.mock(SqlReviewDao.class);
+        Mockito.when(mockReviewDao.createReview(review)).thenReturn(true);
+        boolean reviewSuccess=mockReviewDao.createReview(review);
+        assertTrue(reviewSuccess);
 
     }
 
